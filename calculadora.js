f29ff6e8aeb3e04d74948b241672c4c2800a7aea
KOKI <bone1910@hotmail.com>
2024-11-16 19:00:24 -0500

    Agregados archivos de dependencias

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+class Calculadora {
+    Suma(a, b) {
+      return a + b;
+    }
+  
+    Resta(a, b) {
+      return a - b;
+    }
+  
+    Producto(a, b) {
+      return a * b;
+    }
+  
+    Division(a, b) {
+      return a / b;
+    }
+  }
+  
+  export default Calculadora;
+  
---
+++ calculadora_text.js
@@ -0,0 +1,17 @@
+// Importar assert desde Chai
+import { assert } from "chai"; // Usamos una importación con llaves
+import Calculadora from './calculadora.js'; // Importamos la clase Calculadora
+
+// Crear una instancia de la clase Calculadora
+const calculadora = new Calculadora();
+
+describe('Prueba unitaria: ', function() {
+  describe('Verificando la función Suma', function() {
+
+    it('Verificando Suma', function() {
+      const result = calculadora.Suma(2, 2);
+      assert.equal(result, 3); // Comprobamos que el resultado sea 2
+    });
+
+  });
+});
