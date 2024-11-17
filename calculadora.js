b84f176cc0a8a0a225b9a58e8cf7f525cfdf829b
KOKI8197 <bone1910@hotmail.com>
2024-11-16 19:29:02 -0500

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,20 @@
 class Calculadora {
     Suma(a, b) {
-        return a + b; 
+      return a + b;
     }
-
+  
     Resta(a, b) {
-        return a - b; 
+      return a - b;
     }
-
+  
     Producto(a, b) {
-        return a * b; 
+      return a * b;
     }
-
+  
     Division(a, b) {
-        if (b === 0) {
-            throw new Error("No se puede dividir por cero");
-        }
-        return a / b; 
+      return a / b;
     }
-}
-
-export default Calculadora;  // Exportación correcta de la clase
+  }
+  
+  export default Calculadora;
+  
